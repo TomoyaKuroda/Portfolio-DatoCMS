804f4e8723f8568c38bffd124127f592cd39ec04
Tomoya <200378857@student.georgianc.on.ca>
2019-11-16 20:38:50 -0500

    Add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
-import Img from 'gatsby-image'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import Layout from "../components/layout"
-
+import * as emailjs from "emailjs-com"
+require('purecss')
 const Contact = ({ data: { contact } }) => (
   <Layout>
     <article className="sheet">
@@ -11,15 +12,72 @@ const Contact = ({ data: { contact } }) => (
       <div className="sheet__inner">
         <h1 className="sheet__title">{contact.title}</h1>
         <p className="sheet__lead">{contact.subtitle}</p>
-        <div className="sheet__gallery">
-          <Img fluid={contact.photo.fluid} />
-        </div>
         <div
           className="sheet__body"
-          dangerouslySetInnerHTML={{
-            __html: contact.bioNode.childMarkdownRemark.html,
-          }}
         />
+        <Formik
+          initialValues={{ sender: '', email: '', message: '' }}
+          validate={values => {
+            const errors = {};
+            if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            // setTimeout(() => {
+            //   alert(JSON.stringify(values, null, 2));
+            //   setSubmitting(false);
+            // }, 400);
+            let template_params = {
+              reply_to: values.email,
+              from_name: values.name,
+              to_name: "Tomoya Kuroda",
+              message_html: values.message,
+            }
+
+            let service_id = "default_service"
+            let template_id = "template_bI7QUFno"
+
+            emailjs
+              .send(
+                service_id,
+                template_id,
+                template_params,
+                process.env.GATSBY_EMAILJS_USERID
+              )
+              .then(res => {
+                console.log(res)
+                alert('Thank you for contacting me!');
+                values.name=''
+                values.email=''
+                values.message=''
+                setSubmitting(false);
+              })
+              .catch(err => {
+                console.error(err)
+              })
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="pure-form pure-form-stacked">
+              <label>Name</label>
+              <Field type="text" name="name" className="pure-input-1" required />
+              <ErrorMessage name="name" component="div" />
+              <label>Email</label>
+              <Field type="email" name="email" className="pure-input-1" required />
+              <ErrorMessage name="email" component="div" />
+              <label>Message</label>
+              <Field type="text" name="message" as='textarea' className="pure-input-1" required />
+              <ErrorMessage name="message" component="div" />
+              <button type="submit" disabled={isSubmitting} className="pure-button pure-button-primary">
+                Submit
+          </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </article>
   </Layout>
@@ -35,16 +93,6 @@ export const query = graphql`
       }
       title
       subtitle
-      photo {
-        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
-          ...GatsbyDatoCmsSizes
-        }
-      }
-      bioNode {
-        childMarkdownRemark {
-          html
-        }
-      }
     }
   }
 `
